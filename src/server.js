142f8aa4e0ad9146d08d36fd16f98bd2ad9e856d
johnnyjuarez <johnnyjuarez1992@gmail.com>
2021-01-05 16:35:10 -0600

    added ssl

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config()
-
+const pg = require('pg');
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
-
+pg.default.ssl = true;
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
