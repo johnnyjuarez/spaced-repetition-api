7770b701e5c90517e311669f92e5012bf3e24b4b
johnnyjuarez <johnnyjuarez1992@gmail.com>
2021-01-05 16:37:25 -0600

    added ssl

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const pg = require('pg');
 const knex = require('knex')
 const app = require('./app')
 const { PORT, DATABASE_URL } = require('./config')
-pg.default.ssl = true;
+pg.defaults.ssl = true;
 const db = knex({
   client: 'pg',
   connection: DATABASE_URL,
